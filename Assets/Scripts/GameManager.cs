f6395917b6d7e24a86a3f24b6fbf50420e00d64f
Isoply <thegreatassasinyt@gmail.com>
2021-06-11 14:24:23 +0200

    Added Shop Animation and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [HideInInspector] public bool isPaused;
+
+    public enum PauseStates { Paused, Unpaused, Change };
+
+    public void ChangePauseState(PauseStates pauseState = PauseStates.Change)
+    {
+        if (pauseState == PauseStates.Change) isPaused = !isPaused;
+        else if (pauseState == PauseStates.Paused) isPaused = true;
+        else if (pauseState == PauseStates.Unpaused) isPaused = false;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    GameManager gameManager;
+
+    bool isPlaying;
+    Animator animator;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    public void StartAnimationBool(Animator newAnimator)
+    {
+        animator = newAnimator;
+
+        isPlaying = !isPlaying;
+        animator.SetBool("isAnimating", true);
+        animator.SetBool("isPlaying", isPlaying);
+
+        if (animator.name == "Shop") gameManager.ChangePauseState(GameManager.PauseStates.Change);
+    }
+}
