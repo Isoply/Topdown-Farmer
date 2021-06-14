87cd2d4600d74d9ab37097dcc354cfa38096cd78
Remon <remon@mail.com>
2021-06-14 18:14:51 +0200

    water geven gefixed en gesplitst in 2 scripts

--- Assets/Scripts/Crops/Grow.cs
+++ Assets/Scripts/Crops/Grow.cs
@@ -2,38 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class WaterGrow : MonoBehaviour
-{ 
+public class Grow : MonoBehaviour
+{
 
     public GameObject plant;
     public float growSpeed;
     public float size;
 
     public bool isGrowing;
-
-
-    public GameObject waterParticle;
     // Start is called before the first frame update
     void Start()
     {
-
         growSpeed = 0.001f;
         size = 0.1f;
-
-        waterParticle.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            waterParticle.SetActive(true);
-        }
-        else
-        {
-            waterParticle.SetActive(false);
-        }
         if (isGrowing)
         {
             if (Input.GetKey(KeyCode.Mouse0))
