041ab5bb2911d17dbf3042f9f7882a4f58ed5e00
MarioPizzabakker <47322833+MarioPizzabakker@users.noreply.github.com>
2021-06-14 14:22:32 +0200

    l

---
+++ Assets/Scripts/Crops.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crops : MonoBehaviour
+{
+    //array
+    Crop[] allCrops =
+    {
+        new Crop("Wheat", 3),
+        new Crop ("Carrots", 9),
+        new Crop ("Corn", 12),
+        new Crop ("Potatoes", 16),
+        new Crop ("Cauliflower", 20),
+        new Crop ("Pumpkin", 25),
+    };
+    
+    
+    void Start()
+    {
+        foreach (var crop in allCrops)
+        {
+            crop.icon = Resources.Load<Sprite>($"icons/{crop.name}");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Crop
+{
+    public string name;
+    public int price;
+    public Sprite icon;
+    
+
+    public Crop(string _name, int _price, Sprite _icon = null)
+    {
+        name = _name;
+        price = _price;
+        icon = _icon;
+        
+    }
+}
