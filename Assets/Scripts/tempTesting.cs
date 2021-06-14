69a3338148f86347435203064cb9653dd54c2775
Isoply <0310017@student.rocvantwente.nl>
2021-06-14 12:57:21 +0200

    Daynight basics

---
+++ Assets/Scripts/tempTesting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tempTesting : MonoBehaviour
+{
+    Image display;
+    Text text;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        display = GetComponentInChildren<Image>();
+        text = GetComponentInChildren<Text>();
+        display.material.color = new Color32(0, 100, 255, 255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        text.text = timer.ToString("00");
+
+        if (timer >= 5 && timer <= 15) display.color = new Color32((byte)(255 * ((timer - 5) / 10)), 100, (byte)(255 - (255 * ((timer - 5) / 10))), 255);
+        if (timer >= 50 && timer <= 60) display.color = new Color32((byte)(255 - (255 * ((timer - 50) / 10))), 100, (byte)(255 * ((timer - 50) / 10)), 255);
+        
+        if (timer >= 60) timer = 0;
+    }
+}
