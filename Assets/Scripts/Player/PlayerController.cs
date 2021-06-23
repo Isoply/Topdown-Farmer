0d1e4e9c5c121ab02dcc81cdc86c4d8089753996
Isoply <thegreatassasinyt@gmail.com>
2021-06-23 13:52:19 +0200

    Playtest 1.6 good to go

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,12 +8,16 @@ public class PlayerController : MonoBehaviour
 
     public Text playerFeedback;
     
-    
-
     [HideInInspector] public Crop curCrop;
     Crop hoveredCrop;
-    [HideInInspector] public bool soilRange;
-    bool barrelRange = false;
+
+    Feedback[] allFeedback =
+    {
+        new Feedback("soil", "E"),
+        new Feedback("barrel", "E"),
+        new Feedback("Crop", "R"),
+    };
+
     GameObject soil;
     Grow grow;
     public GameObject crop;
@@ -21,31 +25,34 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         player = GameObject.FindObjectOfType<Player>();
+        playerFeedback.text = "";
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E)) PlantCrop();
         if (grow != null && Input.GetKeyDown(KeyCode.R)) HarvestCrop();
-        if (Input.GetKey(KeyCode.Space) && barrelRange) TakeFromBarrel();
+        if (Input.GetKey(KeyCode.E) && GetFeedback("Barrel").range) TakeFromBarrel();
+        if (playerFeedback.text != "")
+        {
+            playerFeedback.transform.position = Camera.main.WorldToScreenPoint(new Vector3(player.transform.position.x, player.transform.position.y + 0.5f, player.transform.position.z));
+        }
     }
 
     public void PlantCrop()
     {
-        if (soilRange == true && soil.transform.childCount <= 0 && player.playerControl.curCrop != null)
+        if (GetFeedback("Soil").range == true && soil.transform.childCount <= 0 && player.playerControl.curCrop != null)
         {
             GameObject newCrop = Instantiate(crop, soil.transform);
             newCrop.transform.position = soil.transform.position;
             newCrop.GetComponent<Grow>().type = player.playerControl.curCrop;
-            
         }
     }
 
     public void HarvestCrop()
     {
-        if (grow.curSize >= grow.endSize && soilRange)
+        if (grow.curSize >= grow.endSize && crop != null)
         {
-           
             if (Input.GetKeyDown(KeyCode.R))
             {
                 player.gameManager.itemManager.ChangeItemAmount(grow.type.item.name, Random.Range(1, player.gameManager.crops.FindCrop(grow.type.item.name).maxRange));
@@ -75,40 +82,69 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Grow>()) grow = other.GetComponent<Grow>();
-        if (other.name.Substring(0, 4) == "Soil")
-        {
-            if (grow != null) soilFeedback.SetActive(false);
 
-            else if (curCrop != null) soilFeedback.SetActive(true);
-            soil = other.gameObject;
-            soilRange = true;
+
+        if (grow != null && grow.curSize >= grow.endSize) playerFeedback.text = GetFeedback("Crop").text;
+        if (other.name.Length >= 4)
+        {
+            if (other.name.Substring(0, 4) == "Soil")
+            {
+                if (curCrop != null) playerFeedback.text = GetFeedback("Soil").text;
+                soil = other.gameObject;
+                GetFeedback("Soil").range = true;
+            }
         }
         if (other.name.Length >= 6)
         {
             if (other.name.Substring(0, 6) == "Barrel")
             {
-                
+                playerFeedback.text = GetFeedback("Barrel").text;
                 hoveredCrop = CheckBarrelType(other.name.Substring(8, (other.name.Length - 8) - 1));
-                barrelRange = true;
+                GetFeedback("Barrel").range = true;
             }
         }
     }
 
     public void OnTriggerExit2D(Collider2D other)
     {
+        if (other.GetComponent<Grow>())
+        {
+            grow = null;
+            playerFeedback.text = "";
+        }
         if (other.name.Length >= 6)
         {
             if (other.name.Substring(0, 6) == "Barrel")
             {
-                barrelRange = false;
-                
+                playerFeedback.text = "";
+                GetFeedback("Barrel").range = false;
             }
         }
         if (other.name.Substring(0, 4) == "Soil")
         {
-            if (curCrop != null) soilFeedback.SetActive(false);
+            if (curCrop != null) playerFeedback.text = "";
             soil = null;
-            soilRange = false;
+            GetFeedback("Soil").range = false;
         }
+    }
+
+    Feedback GetFeedback(string _name)
+    {
+        foreach (var feedback in allFeedback) if (feedback.name.ToLower() == _name.ToLower()) return feedback;
+        return null;
+    }
+}
+
+public class Feedback
+{
+    public string name;
+    public string text;
+    public bool range;
+
+    public Feedback(string _name, string _text, bool _range = false)
+    {
+        name = _name;
+        text = _text;
+        range = _range;
     }
 }
