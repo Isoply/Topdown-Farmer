6ef9e2c7ffa4144639d92eb69197eabcd346fd94
Isoply <thegreatassasinyt@gmail.com>
2021-06-23 18:57:36 +0200

    fixed basic errors

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -21,7 +21,7 @@ public class ItemManager : MonoBehaviour
 
         //All recipes
         allRecipes.Add(new Item("Bread", 15, new Ingrediant(GetItem("Wheat"), 3)));
-        allRecipes.Add(new Item("Carrot Cake", 50, new Ingrediant(GetItem("Wheat"), 2), new Ingrediant(GetItem("Carrots"), 2)));
+        allRecipes.Add(new Item("Strawberry Cake", 50, new Ingrediant(GetItem("Wheat"), 2), new Ingrediant(GetItem("Strawberries"), 2)));
 
         foreach (var recipe in allRecipes) allSlots.Add(new Slot(recipe.name, recipe));
         UpdateIcons();
