37109e5461af7c81a1bd0cd71b753d73ede6952f
Isoply <thegreatassasinyt@gmail.com>
2021-06-23 20:05:16 +0200

    fixes and updates

--- Assets/Scripts/Crops/Grow.cs
+++ Assets/Scripts/Crops/Grow.cs
@@ -9,14 +9,16 @@ public class Grow : MonoBehaviour
     public float growSpeed = 0.18f;
     public float endSize = 1;
     [HideInInspector] public float curSize = 0.1f;
-    float overwatered = 0.60f;
+    float overwatered = 0.35f;
     public Sprite[] cropSprites;
     float timer;
     float lastTime;
-    bool isGrowing;
+    [HideInInspector] public bool isGrowing;
     private void Start()
     {
         gameManager = GameObject.FindObjectOfType<GameManager>();
+        transform.localScale = new Vector2(0.15f, 0.15f);
+        UpdateSprite(); 
     }
     // Update is called once per frame
     void Update()
@@ -27,7 +29,7 @@ public class Grow : MonoBehaviour
         if (timer >= overwatered)
         {
             isGrowing = false;
-            GetComponent<SpriteRenderer>().color = new Color(255, 0, 0, 255);
+            GetComponent<SpriteRenderer>().color = new Color(125, 0, 0, 255);
         }
         else if (timer > 0.15f) isGrowing = true;
 
@@ -39,39 +41,23 @@ public class Grow : MonoBehaviour
         if (curSize <= endSize && isGrowing == true)
         {
             curSize += growSpeed * Time.deltaTime;
-            transform.localScale = new Vector2(curSize, curSize);
+            if (curSize >= endSize) isGrowing = false;
+            //transform.localScale = new Vector2(curSize, curSize);
         }
-         if(curSize >= (endSize / 2))
-         {
-            if(type.item.name == "Wheat")
-            GetComponent<SpriteRenderer>().sprite = cropSprites[0];
-            if (type.item.name == "Potatoes")
-            GetComponent<SpriteRenderer>().sprite = cropSprites[3];
-            if (type.item.name == "Strawberry")
-            GetComponent<SpriteRenderer>().sprite = cropSprites[6];
+        UpdateSprite();
+    }
 
+    void UpdateSprite()
+    {
+        int startSize = 0;
+        if (type.item.name == "Wheat") startSize = 0;
+        if (type.item.name == "Potatoes") startSize = 3;
+        if (type.item.name == "Strawberries") startSize = 6;
 
-        }
-         if (curSize >= (endSize / 1.50))
-         {
-            if (type.item.name == "Wheat")
-                GetComponent<SpriteRenderer>().sprite = cropSprites[1];
-            if (type.item.name == "Potatoes")
-                GetComponent<SpriteRenderer>().sprite = cropSprites[4];
-            if (type.item.name == "Strawberry")
-                GetComponent<SpriteRenderer>().sprite = cropSprites[7];
-        }
-        if (curSize >= endSize)
-        {
-            timer = 0;
-            isGrowing = false;
-            if (type.item.name == "Wheat")
-                GetComponent<SpriteRenderer>().sprite = cropSprites[2];
-            if (type.item.name == "Potatoes")
-                GetComponent<SpriteRenderer>().sprite = cropSprites[5];
-            if (type.item.name == "Strawberry")
-                GetComponent<SpriteRenderer>().sprite = cropSprites[8];
-        }
+        if (curSize >= endSize) GetComponent<SpriteRenderer>().sprite = cropSprites[startSize + 2];
+        else if (curSize >= (endSize / 1.50)) GetComponent<SpriteRenderer>().sprite = cropSprites[startSize + 1];
+        else if (curSize >= (endSize / 2)) GetComponent<SpriteRenderer>().sprite = cropSprites[startSize];
+        else GetComponent<SpriteRenderer>().sprite = cropSprites[startSize];
     }
 
     private void OnParticleCollision(GameObject other)
