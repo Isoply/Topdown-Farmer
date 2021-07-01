b892d726b8e8bfddeca65e1a1cd03bd4685353fc
MarioPizzabakker <47322833+MarioPizzabakker@users.noreply.github.com>
2021-07-01 15:30:20 +0200

    start goal reset

--- Assets/Scripts/Variables/Goals.cs
+++ Assets/Scripts/Variables/Goals.cs
@@ -6,7 +6,7 @@ public class Goals
 {
     public List<Day> allGoals = new List<Day>
     {
-        new Day(1, 0),
+        new Day(1, 10),
         new Day(5, 2000),
         new Day(10, 5000),
         new Day(15, 15000),
