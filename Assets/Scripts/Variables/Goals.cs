a7626af8abfea332ec3fa186f3bc8dbde5683c1d
MarioPizzabakker <47322833+MarioPizzabakker@users.noreply.github.com>
2021-07-01 15:29:34 +0200

    sprite changes en goals balance

--- Assets/Scripts/Variables/Goals.cs
+++ Assets/Scripts/Variables/Goals.cs
@@ -6,8 +6,8 @@ public class Goals
 {
     public List<Day> allGoals = new List<Day>
     {
-        new Day(1, 10),
-        new Day(5, 1500),
+        new Day(1, 0),
+        new Day(5, 2000),
         new Day(10, 5000),
         new Day(15, 15000),
         new Day(20, 30000),
